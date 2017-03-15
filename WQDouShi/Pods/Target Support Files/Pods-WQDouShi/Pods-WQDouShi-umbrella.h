9d3d92b442fa5684f93a27803ca667078eff0b83
linweiqiang <1145530006@qq.com>
2017-03-15 15:49:42 +0800

    添加Pod

---
+++ WQDouShi/Pods/Target Support Files/Pods-WQDouShi/Pods-WQDouShi-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_WQDouShiVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WQDouShiVersionString[];
+
---
+++ WQDouShi/Pods/Target Support Files/ZFPlayer/ZFPlayer-umbrella.h
@@ -0,0 +1,23 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "ASValuePopUpView.h"
+#import "ASValueTrackingSlider.h"
+#import "NSObject+Extend.h"
+#import "UIImageView+ZFCache.h"
+#import "UINavigationController+ZFPlayerRotation.h"
+#import "UITabBarController+ZFPlayerRotation.h"
+#import "UIView+CustomControlView.h"
+#import "UIViewController+ZFPlayerRotation.h"
+#import "MMMaterialDesignSpinner.h"
+#import "ZFBrightnessView.h"
+#import "ZFFullScreenViewController.h"
+#import "ZFPlayer.h"
+#import "ZFPlayerControlView.h"
+#import "ZFPlayerModel.h"
+#import "ZFPlayerView.h"
+
+FOUNDATION_EXPORT double ZFPlayerVersionNumber;
+FOUNDATION_EXPORT const unsigned char ZFPlayerVersionString[];
+
