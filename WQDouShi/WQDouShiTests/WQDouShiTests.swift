91c2c1c2e77965aafe01b0d7bce33ab746e8ded3
linweiqiang <1145530006@qq.com>
2017-03-15 15:45:47 +0800

    初始化项目

---
+++ WQDouShi/WQDouShiTests/WQDouShiTests.swift
@@ -0,0 +1,36 @@
+//
+//  WQDouShiTests.swift
+//  WQDouShiTests
+//
+//  Created by yoke2 on 2017/3/15.
+//  Copyright © 2017年 yoke121. All rights reserved.
+//
+
+import XCTest
+@testable import WQDouShi
+
+class WQDouShiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
